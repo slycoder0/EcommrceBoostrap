60b10f3bd0c7bd1096d4cd90a22c83312e760558
DanielMelo0 <danielmelo9001@gmail.com>
2024-08-31 01:05:56 -0300

    React Version

---
+++ CrystalReact/src/Pages/HomePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Header from '../Components/Home/Header/Header';
+const HomePage = () => {
+  return (
+    <>
+      <Header />
+    </>
+  );
+};
+
+export default HomePage;
